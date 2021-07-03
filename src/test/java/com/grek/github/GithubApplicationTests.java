c546b5e621051182a67d4d4cab8cdf4d464b1c6f
Grzegorz <grzegorzjuliank@gmail.com>
2021-07-03 18:08:20 +0200

    start commit

---
+++ src/test/java/com/grek/github/GithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grek.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
